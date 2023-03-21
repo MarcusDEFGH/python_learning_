59226b39197b0672ca6f6bc9c82e61fcd0906b97
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2023-03-21 11:11:42 -0300

    Add files via upload
    Improvement (same result with less lines of code) in progress, comments better organized

--- sorteio-cardapio-dia-dieta.py
+++ sorteio-cardapio-dia-dieta.py
@@ -9,18 +9,7 @@ jantar  = ['cuscuz c/ ovo', 'bolinho batata airfryer', 'tapioca', 'papa', 'sandu
 refeicoes_dia = cafe, lanche_manha, almoco, lanche_tarde, jantar
 dias_semana = ['seg', 'ter', 'qua', 'qui', 'sex', 'sab', 'dom']
 
-# cardapio_dia = [(C,LM,AL,LT,J) for C in cafe for LM in lanche_manha for AL in almoco for LT in lanche_tarde for J in jantar]
-# print(cardapio_dia)
-
-#
-# for dia in dias_semana:
-#     for i in range(5):
-#         for ref in refeicoes_dia:
-#             menu_dia = f'{dia}: {random.sample(ref,5)}'
-#     print(menu_dia)
-
-#consegui. a interação desejada na ordem certa.
-refss = []
+#consegui. a iteração desejada na ordem certa.
 for dia in dias_semana:
     menu_dia = []
     for ref in refeicoes_dia:
@@ -30,15 +19,40 @@ for dia in dias_semana:
     #     f'\033[94m{dia}\033[m:')
     # for a in range(5):
     #         print(f'{menu_dia[a]}')
-    #como fazer isso com list comprehension e com for loop pra essa parte de 'pegar' só o item str
-    # (formatado bonitinho, sem " ou [
-    # de cada refeiçao?
 
+    #como fazer isso abaixo com list comprehension e com for loop pra essa parte de 'pegar' só o item str
+    # (formatado bonitinho, sem " ou [ de cada refeiçao?
     print(f'\033[94m{dia}\033[m:{menu_dia[0]}, {menu_dia[1]}, {menu_dia[2]}, {menu_dia[3]}, {menu_dia[4]}')
 
+    #tentando mesmo resultado através de  list comprehension: e com for loop pra essa parte de 'pegar' só o item str
+    # (formatado bonitinho, sem " ou [ de cada refeiçao?
+
+for dia in dias_semana:
+    menu_dia = [
+        (f'\033[94m{dia}\033[m:'
+         f'{refs_sorteadas}' for random.sample(refeicao, 1) for refeicao in refeicoes_dia)
+    ]
+    # for ref in refeicoes_dia:
+    #     ref_sorteada = random.sample(ref, 1)[0]
+    #     menu_dia.append(ref_sorteada)
+
+    # print(
+    #     f'\033[94m{dia}\033[m:')
+    # for a in range(5):
+    #         print(f'{menu_dia[a]}')
+#adicionar a regra: se papa de aveia aparecer como prato do cafe da manhã, nao pode aparecer de novo como jantar
 
 
+#           1   ----------------------------------------------------------------TENTATIVAS ANTERIORES------------------
+# cardapio_dia = [(C,LM,AL,LT,J) for C in cafe for LM in lanche_manha for AL in almoco for LT in lanche_tarde for J in jantar]
+# print(cardapio_dia)
 
+# for dia in dias_semana:
+#     for i in range(5):
+#         for ref in refeicoes_dia:
+#             menu_dia = f'{dia}: {random.sample(ref,5)}'
+#     print(menu_dia)
+#           2   --------------------------------------------------------
 # funcionou o cód abaixo, mas retorna sequencia de ref de cada um dos 5 tipos, se segunda ate domingo,
 # e não o menu c lista com 1 de cada tipo por dia da semana:
 
@@ -46,8 +60,7 @@ for dia in dias_semana:
 #     for dia in dias_semana:
 #         print(f'{dia}: {random.sample(ref, 1)}')
 # print(card_dia)
-
-
+#             3--------------------------------------------------------
 # for dia_semana in range(7):
 #     for refeicao in refeicoes_dia:
 #         card_dia.append(random.sample(refeicao,1))
@@ -56,4 +69,3 @@ for dia in dias_semana:
     #     cardapio_semana = random.sample(refeicao, 5)
     #
     # print(cardapio_semana)
-
